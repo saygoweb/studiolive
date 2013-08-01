7eb6bece347b41ea6e49177f34f504306b7d3793
cambell-prince <cambell.prince@gmail.com>
2013-08-01 23:38:40 +0700

    wip: Simplify SceneModel implementation.

--- src/api/models/SceneModel.php
+++ src/api/models/SceneModel.php
@@ -4,29 +4,12 @@ namespace models;
 
 use models\mapper\ArrayOf;
 
-class SceneModel extends mapper\MapperSubModel
+class SceneModel
 {
-	public function __construct($showId, $id = '') {
-		$this->actions = new ArrayOf(ArrayOf::OBJECT, function($data) {
-			return new ActionModel();
-		});
-		$mapper = ShowModelMongoMapper::instance();
-		parent::__construct($mapper, $showId, 'scenes', $id);
+	public function __construct() {
+		$this->actions = new ArrayOf(ArrayOf::VALUE);
 	}
 	
-	public function keyString() {
-		return $this->name;
-	}
-
-	/**
-	 * Removes the record $id from the collection.
-	 * @param string $id
-	 * @return int The number of records removed from the collection.
-	 */
-	public static function remove($showId, $id) {
-		return ShowModelMongoMapper::instance()->removeSubDocument($showId, 'scenes', $id);
-	}
-
 	public $id;
 
 	public $name;
--- src/api/models/ShowModel.php
+++ src/api/models/ShowModel.php
@@ -2,6 +2,8 @@
 
 namespace models;
 
+use models\mapper\MongoMapper;
+
 use models\mapper\ArrayOf;
 
 use models\mapper\Id;
@@ -29,14 +31,50 @@ class ShowModel extends mapper\MapperModel
 	public function read($id) {
 		$result = parent::read($id);
 		// Check the scenesIndex for sanity.
-		if (count($this->scenes) != count($this->scenesIndex->data)) {
-			error_log("ScenesIndex does not match");
-			$this->scenesIndex->data = array_keys($this->scenes);
-			$this->write();
-		}
+// 		if (count($this->scenes) != count($this->scenesIndex->data)) {
+// 			error_log("ScenesIndex does not match");
+// 			$this->scenesIndex->data = array_keys($this->scenes);
+// 			$this->write();
+// 		}
 		return $result;
 	}
 
+	/**
+	 * Reads a SceneModel 
+	 * @param string $showId
+	 * @param string $id
+	 * @return SceneModel
+	 */
+	public static function readScene($showId, $id) {
+		$mapper = ShowModelMongoMapper::instance();
+		$sceneModel = new SceneModel();
+		$mapper->readSubDocument($sceneModel, $showId, 'scenes', $id);
+		return $sceneModel;
+	}
+	
+	/**
+	 * Writes (Updates) a SceneModel
+	 * @param string $showId
+	 * @param SceneModel $sceneModel
+	 * @return string
+	 */
+	public static function writeScene($showId, $sceneModel) {
+		$mapper = ShowModelMongoMapper::instance();
+		$id = ShowModelMongoMapper::mongoID($sceneModel->name);
+		$mapper->write($sceneModel, $id, MongoMapper::ID_IN_KEY, $showId, 'scenes');
+		return $id;
+	}
+	
+	/**
+	 * Removes a SceneModel
+	 * @param string $showId
+	 * @param string $id
+	 */
+	public static function removeScene($showId, $id) {
+		$mapper = ShowModelMongoMapper::instance();
+		return $mapper->removeSubDocument($showId, 'scenes', $id);
+	}
+	
 	public $id;
 
 	public $name;
