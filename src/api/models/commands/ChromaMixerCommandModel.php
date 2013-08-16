ac8552608ce09892513d38901e556e4b8f1f9ac9
cambell-prince <cambell.prince@gmail.com>
2013-08-16 08:39:57 +0700

    Chroma command

--- src/api/models/commands/ChromaMixerCommandModel.php
+++ src/api/models/commands/ChromaMixerCommandModel.php
@@ -4,7 +4,20 @@ namespace models\commands;
 
 class ChromaMixerCommandModel extends InputCommandModel
 {
-
+	public function casparCommandIn() {
+		$result = sprintf(
+				'MIXER %d-%d CHROMA %s %.3f %.3f',
+				$this->channel, $this->layer,
+				$this->chroma, $this->low, $this->high
+		);
+		return $result;
+	}
+	
+	public function casparCommandOut() {
+		$result = sprintf('MIXER %d-%d CHROMA none', $this->channel, $this->layer);
+		return $result;
+	}
+	
 	/**
 	 * The chroma color. Either Blue or Green
 	 * @var string
