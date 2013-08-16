7b9aae529e59232e0befef741e952b633cadc4f2
cambell-prince <cambell.prince@gmail.com>
2013-08-16 07:39:09 +0700

    Geometry (FILL) can Opacity commands

--- src/api/models/commands/GeometryMixerCommandModel.php
+++ src/api/models/commands/GeometryMixerCommandModel.php
@@ -4,7 +4,30 @@ namespace models\commands;
 
 class GeometryMixerCommandModel extends InputCommandModel
 {
-
+	
+	public function casparCommandIn() {
+		// TODO These need to come from somewhere else. CP 2013-08
+		$channelWidth = 1280;
+		$channelHeight = 720;
+		
+		$x = $this->x / $channelWidth;
+		$y = $this->y / $channelHeight;
+		$w = $this->w / $channelWidth;
+		$h = $this->h / $channelHeight;
+		
+		$result = sprintf(
+			'MIXER %d-%d FILL %.6f %.6f %.6f %.6f 12', 
+			$this->channel, $this->layer, 
+			$x, $y, $w, $h
+		);
+		return $result;
+	}
+	
+	public function casparCommandOut() {
+		$result = sprintf('MIXER %d-%d FILL 0 0 1 1 12', $this->channel, $this->layer);
+		return $result;
+	}
+	
 	public $x;
 	public $y;
 	public $w;
--- src/api/models/commands/OpacityMixerCommandModel.php
+++ src/api/models/commands/OpacityMixerCommandModel.php
@@ -4,7 +4,21 @@ namespace models\commands;
 
 class OpacityMixerCommandModel extends InputCommandModel
 {
-
+	public function casparCommandIn() {
+		$result = sprintf(
+			'MIXER %d-%d OPACITY %.2f 12',
+			$this->channel, $this->layer,
+			$this->opacity
+		);
+		return $result;
+	}
+	
+	public function casparCommandOut() {
+		$result = sprintf('MIXER %d-%d OPACITY 1 12', $this->channel, $this->layer);
+		return $result;
+	}
+	
+	
 	public $opacity;
 	
 }
