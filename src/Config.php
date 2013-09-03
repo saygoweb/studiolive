8e9ff20250bb330e914e8e354e3713e243ded0b2
cambell-prince <cambell.prince@gmail.com>
2013-09-03 20:32:23 +0700

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -1,9 +1,20 @@
 <?php
-define('VERSION', '0.0.0');
-define('BUILD_DATE', '8 October 2013');
-define('USE_LIBS', false);
+// Set the CASPAR_HOST and CASPAR_PORT to match your system setup.  Most likely to be localhost.
+define('CASPAR_HOST', 'localhost');
+define('CASPAR_PORT', '5250');
+
 define('USE_BOOT', true);
 
+// -------------------------------------------------------------------
+// You shouldn't need to change anything below this line.
+// -------------------------------------------------------------------
+if (!defined('SL_DATABASE')) {
+	define('SL_DATABASE', 'studiolive');
+}
+
+// -------------------------------------------------------------------
+// Changing anything below this line may well break things quite badly.
+// -------------------------------------------------------------------
 if (USE_LIBS) {
 	$pharFile = realpath(dirname(__FILE__) . '/api/studiolive.phar');
 	$rootPath = 'phar://' . $pharFile . '/';
@@ -21,12 +32,9 @@ if (USE_LIBS) {
 	require_once(API_PATH . 'libraries/palaso/Loader.php');
 }
 
+define('USE_LIBS', false);
 
-define('CASPAR_HOST', 'videotest');
-define('CASPAR_PORT', '5250');
-
-if (!defined('SL_DATABASE')) {
-	define('SL_DATABASE', 'studiolive');
-}
+define('VERSION', '0.0.0');
+define('BUILD_DATE', '8 October 2013');
 
 ?>
