63334fc72070d1d59e1af48e8d570956170362e3
cambell-prince <cambell.prince@gmail.com>
2013-09-09 21:28:00 +0700

    snap: Fix tests.

--- test/php/commands/CasparSnapCommand_Test.php
+++ test/php/commands/CasparSnapCommand_Test.php
@@ -21,7 +21,7 @@ class CasparSnapTestConnection {
 			$this->dateTime = new \DateTime();
 			$filePath = sprintf("%s%s%s.png", CASPAR_PATH_SNAP, DIRECTORY_SEPARATOR, $this->dateTime->format('Ymd\THis'));
 			file_put_contents($filePath, 'Fake data for Studio Live tests');
-			return $this->response;
+			return array($this->response);
 		}
 	}
 	
@@ -29,23 +29,31 @@ class CasparSnapTestConnection {
 
 class TestCasparSnapCommand extends UnitTestCase {
 
+	private $_environment;
+	
 	function __construct() {
-		$casparTestConnection = new CasparSnapTestConnection();
-		//CasparConnection::connect(CASPAR_HOST, CASPAR_PORT, $casparTestConnection);
+		$this->_environment = new CasparSnapTestConnection();
+		CasparConnection::connect(CASPAR_HOST, CASPAR_PORT, $this->_environment);
 	}
 	
 	function testSnapCommand() {
 		$command = new CasparSnapCommand();
 		$result = $command->snap(1);
-		var_dump($result);
 		
-		$this->assertTrue(false);
 		// Test result is correct url for snap file.
-		
+		$expectedFileName = $this->_environment->dateTime->format('Ymd\THis') . '.png';
+		$expectedFilePath = '/images/snap/' . $expectedFileName;
+		$this->assertEqual($expectedFilePath, $result);
+
 		// Test that file has been deleted off Caspar.
+		$exists = file_exists(CASPAR_PATH_SNAP . DIRECTORY_SEPARATOR . $expectedFileName);
+		$this->assertFalse($exists, "File $expectedFileName should have been deleted from Caspar");
 		
 		// Test that file is present in snaps folder.
+		$exists = file_exists(SL_PATH_SNAP . DIRECTORY_SEPARATOR . $expectedFileName);
+		$this->assertTrue($exists);
 		
+		unlink(SL_PATH_SNAP . DIRECTORY_SEPARATOR . $expectedFileName);
 	}
 	
 }
